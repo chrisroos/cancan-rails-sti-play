4e8fb6d45a927325915a7858d7a9725157e967d3
Chris Roos <chris@seagul.co.uk>
2013-08-08 17:31:00 +0100

    Empty Rails 4.0 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CancanRailsStiExampleProject::Application.config.secret_key_base = '558dff9cbf2a1e8cdac4a6d255f2399f2b55ea00435cad0ec923cad675948651a9fcde28e5f973766842e2a25c23fb4c4c7632a118f5dcc7748fc645c8c84f07'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CancanRailsStiExampleProject::Application.config.session_store :cookie_store, key: '_cancan-rails-sti-example-project_session'
